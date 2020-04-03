f3c7a7acc1bc8962c991a09949a67f51b84c9ce1
Vlad-Vargan <blach.smith.2000@gmail.com>
2020-04-04 00:11:21 +0300

    first shit

---
+++ src/bot.py
@@ -0,0 +1,135 @@
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler)
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+from etc import text
+from os import getcwd, environ
+import logging
+from random import randint
+
+from database import DbInterface
+from user import UserManager, User
+from variables import *
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+db = DbInterface(getcwd() + "/database.db")
+
+UM = UserManager()
+
+def get_games_id(update,context):
+    answer = UM.currentUsers[update.message.chat.id].answers
+    game_id = []
+    game_id += db.getGames(answer[0],answer[1],answer[2],answer[3],answer[4])
+
+    if None in answer:
+        keys = [[0,1,2],[0,1],[0,1,2],[0,1],[0,1]]
+        data = [answer[0],answer[1],answer[2],answer[3],answer[4]]
+        for j in range(5):
+            if answer[j] == None:
+                for i in keys[j]:
+                    data[j] = i
+                    game_id += db.getGames(*data)
+        game_id = sorted(list(set(game_id)))
+    
+    return game_id
+
+def start_query(update, context):
+    reply_keyboard = [[text["games"]],[text["random"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["start_games"], reply_markup = markup)
+    return GAMES
+
+def game_start(update,context):
+    if update.message.text == text["games"]:
+        return a_type(update,context)
+    elif update.message.text == text["random"]:
+        return rand(update,context)
+
+def rand(update,context):
+    reply_keyboard = [[text["back"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(games[randint(1,53)], reply_markup = markup)
+    return BACK
+
+def back(update,context):
+    if update.message.text == text["back"]:
+        return start_query(update, context)
+
+
+def back_answer(update,context):
+    massage = update.message.text
+    lang = UM.currentUsers[update.message.chat.id].lang
+    if massage == text["back"]:
+        return result(update,context)
+    elif massage == text["menu"]:
+        return start_query(update, context)
+
+
+def start(update, context):
+    update.message.reply_text(text["start"], reply_markup = ReplyKeyboardRemove())
+    pass
+
+
+def admin(update, context):
+    pass
+
+
+def done(update, context):
+    update.message.reply_text('END')
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    updater = Updater(environ.get("API_KEY"), use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+    necessary_hendlers = [CommandHandler('stop', done),
+                          CommandHandler('start', start),
+                          CommandHandler('admin', admin)]
+    # Add conversation handler with the states CHOOSE_LANG, ASK_AGE, ASK_AMOUNT, ASK_LOCATION, ASK_PROPS and START_QUERY
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            ADMIN: [MessageHandler(Filters.text, admin_password),*necessary_hendlers],
+            ADMIN_PASSWORD: [MessageHandler(Filters.text, new_password),*necessary_hendlers],
+            CHOOSE_LANG: [MessageHandler(Filters.text, set_lang),*necessary_hendlers],
+            CHECK_PASSWORD: [MessageHandler(Filters.text, check_password),*necessary_hendlers],
+            GAMES: [MessageHandler(Filters.text, game_start),*necessary_hendlers],
+            BACK: [MessageHandler(Filters.text, back),*necessary_hendlers],
+            ASK_TYPE: [MessageHandler(Filters.text, a_type),*necessary_hendlers],
+            ASK_AGE: [MessageHandler(Filters.text, a_age),*necessary_hendlers],
+            ASK_AMOUNT: [MessageHandler(Filters.text, a_amount),*necessary_hendlers],
+            ASK_LOCATION: [MessageHandler(Filters.text, a_location),*necessary_hendlers],
+            ASK_PROPS: [MessageHandler(Filters.text, a_props),*necessary_hendlers],
+            RESULT: [MessageHandler(Filters.text, result),*necessary_hendlers],
+            ANSWER: [MessageHandler(Filters.text, final_answer),*necessary_hendlers],
+            BACK_ANSWER: [MessageHandler(Filters.text, back_answer),*necessary_hendlers],
+        },
+
+        fallbacks=[CommandHandler('stop', done)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/commands/qestions.py
@@ -0,0 +1,129 @@
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+from variables import *
+
+def a_type(update,context):
+    UM.create_user(User(update.message.chat.id,update.message.chat.username))
+
+    UM.currentUsers[update.message.chat.id].set_flag(1)
+    reply_keyboard = [[text["team_building"]],[text["ice_breaker"]],[text["timefiller"]],
+                      [text["any"],text["back"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["ask_type"], reply_markup = markup)
+    return ASK_AGE
+
+def a_age(update,context):
+    massage = update.message.text
+    if massage == text["team_building"]:
+        UM.currentUsers[update.message.chat.id].take_answer(0,0)
+    elif massage == text["ice_breaker"]:
+        UM.currentUsers[update.message.chat.id].take_answer(0,1)
+    elif massage == text["timefiller"]:
+        UM.currentUsers[update.message.chat.id].take_answer(0,2)
+    elif massage == text["back"] and UM.currentUsers[update.message.chat.id].flag == 1:
+        return start_query(update,context)
+    elif massage == text["any"]:
+        pass
+    UM.currentUsers[update.message.chat.id].set_flag(2)
+
+    reply_keyboard = [[text["6-12"],text["12+"]],
+                      [text["any"],text["back"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["ask_age"], reply_markup = markup)
+    return ASK_AMOUNT
+
+def a_amount(update,context):
+    massage = update.message.text
+    if massage == text["6-12"]:
+        UM.currentUsers[update.message.chat.id].take_answer(1,0)
+    elif massage == text["12+"]:
+        UM.currentUsers[update.message.chat.id].take_answer(1,1)
+    elif massage == text["back"] and UM.currentUsers[update.message.chat.id].flag == 2:
+        return a_type(update,context)
+    elif massage == text["any"]:
+        pass
+    UM.currentUsers[update.message.chat.id].set_flag(3)
+
+    reply_keyboard = [[text["up to 5"],text["5-20"],text["20+"]],
+                      [text["any"],text["back"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["ask_amount"], reply_markup = markup)
+    return ASK_LOCATION
+
+def a_location(update,context):
+    massage = update.message.text
+    if massage == text["up to 5"]:
+        UM.currentUsers[update.message.chat.id].take_answer(2,0)
+    elif massage == text["5-20"]:
+        UM.currentUsers[update.message.chat.id].take_answer(2,1)
+    elif massage == text["20+"]:
+        UM.currentUsers[update.message.chat.id].take_answer(2,2)
+    elif massage == text["back"] and UM.currentUsers[update.message.chat.id].flag == 3:
+        return a_age(update,context)
+    elif massage == text["any"]:
+        pass
+    UM.currentUsers[update.message.chat.id].set_flag(4)
+
+    reply_keyboard = [[text["outside"],text["inside"]],
+                      [text["any"],text["back"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["ask_location"], reply_markup = markup)
+    return ASK_PROPS
+
+def a_props(update,context):
+    massage = update.message.text
+    if massage == text["outside"]:
+        UM.currentUsers[update.message.chat.id].take_answer(3,0)
+    elif massage == text["inside"]:
+        UM.currentUsers[update.message.chat.id].take_answer(3,1)
+    elif massage == text["back"] and UM.currentUsers[update.message.chat.id].flag == 4:
+        return a_amount(update,context)
+    elif massage == text["any"]:
+        pass
+    UM.currentUsers[update.message.chat.id].set_flag(5)
+
+    reply_keyboard = [[text["yes"],text["no"]],
+                      [text["any"],text["back"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["ask_props"], reply_markup = markup)
+    return RESULT
+
+def result(update,context):
+    massage = update.message.text
+    if massage == text["no"]:
+        UM.currentUsers[update.message.chat.id].take_answer(4,0)
+    elif massage == text["yes"]:
+        UM.currentUsers[update.message.chat.id].take_answer(4,1)
+    elif massage == text["back"] and UM.currentUsers[update.message.chat.id].flag == 5:
+        return a_location(update,context)
+    elif massage == text["any"]:
+        pass
+    UM.currentUsers[update.message.chat.id].set_flag(6)
+
+    game_id = get_games_id(update,context)
+    update.message.reply_text(game_id)
+    buttons_language = "en" if lang == 1 else "ru"
+    reply_keyboard = [[names[i][buttons_language]] for i in game_id]
+    reply_keyboard.append([text["back"],text["menu"]])
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(text["answer"], reply_markup = markup)
+    return ANSWER
+
+def final_answer(update,context):
+    massage = update.message.text
+    if massage == text["back"] and UM.currentUsers[update.message.chat.id].flag == 6:
+        return a_props(update,context)
+    elif massage == text["menu"]:
+        UM.delete_user(update.message.chat.id)
+        return start_query(update, context)
+    UM.currentUsers[update.message.chat.id].set_flag(7)
+
+    solution = None
+    for key in names:
+        if massage == names[key]:
+            solution = key
+            break
+    reply_keyboard = [[text["back"],text["menu"]]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(games[solution], reply_markup = markup)
+    return BACK_ANSWER
