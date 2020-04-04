2e2714454f5a30a1f9b1d33b8b7e660438025ad7
Alexey <almarka03@gmail.com>
2020-04-04 18:44:18 +0300

    correct Bot.py

--- src/bot.py
+++ src/bot.py
@@ -7,54 +7,46 @@ from random import randint
 from database import DbInterface
 from user import UserManager, User
 
-
 CHOOSE_LANG, CHECK_PASSWORD, ADMIN, GAMES, BACK, ASK_TYPE,\
 ASK_AGE, ASK_AMOUNT, ASK_LOCATION, ASK_PROPS, RESULT,\
 ANSWER,BACK_ANSWER, ADMIN_PASSWORD = range(14)
 
+updater = Updater(environ.get("API_KEY"), use_context=True)
 
-def main():
-    updater = Updater(environ.get("API_KEY"), use_context=True)
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
+necessary_hendlers = [CommandHandler('stop', done),
+                      CommandHandler('start', start),
+                      CommandHandler('admin', admin)]
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-    necessary_hendlers = [CommandHandler('stop', done),
-                          CommandHandler('start', start),
-                          CommandHandler('admin', admin)]
+# Payment logic
+dispatcher.add_handler(CommandHandler('pay', pay))
+dispatcher.add_handler(PreCheckoutQueryHandler(precheckout_callback))
+dispatcher.add_handler(MessageHandler(Filters.successful_payment, successful_payment_callback))
 
-    # Payment logic
-	dispatcher.add_handler(CommandHandler('pay', pay))
-	dispatcher.add_handler(PreCheckoutQueryHandler(precheckout_callback))
-	dispatcher.add_handler(MessageHandler(Filters.successful_payment, successful_payment_callback))
+# Add conversation handler with the states CHOOSE_LANG, ASK_AGE, ASK_AMOUNT, ASK_LOCATION, ASK_PROPS and START_QUERY
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        ADMIN: [MessageHandler(Filters.text, admin_password),*necessary_hendlers],
+        ADMIN_PASSWORD: [MessageHandler(Filters.text, new_password),*necessary_hendlers],
+        CHOOSE_LANG: [MessageHandler(Filters.text, set_lang),*necessary_hendlers],
+        CHECK_PASSWORD: [MessageHandler(Filters.text, check_password),*necessary_hendlers],
+        GAMES: [MessageHandler(Filters.text, game_start),*necessary_hendlers],
+        BACK: [MessageHandler(Filters.text, back),*necessary_hendlers],
+        ASK_TYPE: [MessageHandler(Filters.text, a_type),*necessary_hendlers],
+        ASK_AGE: [MessageHandler(Filters.text, a_age),*necessary_hendlers],
+        ASK_AMOUNT: [MessageHandler(Filters.text, a_amount),*necessary_hendlers],
+        ASK_LOCATION: [MessageHandler(Filters.text, a_location),*necessary_hendlers],
+        ASK_PROPS: [MessageHandler(Filters.text, a_props),*necessary_hendlers],
+        RESULT: [MessageHandler(Filters.text, result),*necessary_hendlers],
+        ANSWER: [MessageHandler(Filters.text, final_answer),*necessary_hendlers],
+        BACK_ANSWER: [MessageHandler(Filters.text, back_answer),*necessary_hendlers],
+    },
+    fallbacks=[CommandHandler('stop', done)]
+)
 
-    # Add conversation handler with the states CHOOSE_LANG, ASK_AGE, ASK_AMOUNT, ASK_LOCATION, ASK_PROPS and START_QUERY
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            ADMIN: [MessageHandler(Filters.text, admin_password),*necessary_hendlers],
-            ADMIN_PASSWORD: [MessageHandler(Filters.text, new_password),*necessary_hendlers],
-            CHOOSE_LANG: [MessageHandler(Filters.text, set_lang),*necessary_hendlers],
-            CHECK_PASSWORD: [MessageHandler(Filters.text, check_password),*necessary_hendlers],
-            GAMES: [MessageHandler(Filters.text, game_start),*necessary_hendlers],
-            BACK: [MessageHandler(Filters.text, back),*necessary_hendlers],
-            ASK_TYPE: [MessageHandler(Filters.text, a_type),*necessary_hendlers],
-            ASK_AGE: [MessageHandler(Filters.text, a_age),*necessary_hendlers],
-            ASK_AMOUNT: [MessageHandler(Filters.text, a_amount),*necessary_hendlers],
-            ASK_LOCATION: [MessageHandler(Filters.text, a_location),*necessary_hendlers],
-            ASK_PROPS: [MessageHandler(Filters.text, a_props),*necessary_hendlers],
-            RESULT: [MessageHandler(Filters.text, result),*necessary_hendlers],
-            ANSWER: [MessageHandler(Filters.text, final_answer),*necessary_hendlers],
-            BACK_ANSWER: [MessageHandler(Filters.text, back_answer),*necessary_hendlers],
-        },
-        fallbacks=[CommandHandler('stop', done)]
-    )
-    
-    dp.add_handler(conv_handler)
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+dp.add_handler(conv_handler)
+dp.add_error_handler(error)
+updater.start_polling()
+updater.idle()
