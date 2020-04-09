9d1b6c1964d67a8499f9dbebad33ee3af8b9d8e9
Elon_Mask <you@example.com>
2020-04-10 00:27:33 +0300

    new DB

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from pprint import PrettyPrinter
 
 class DbInterface:
     def __init__(self, path):
@@ -51,8 +52,48 @@ class DbInterface:
         self.cursor.execute(sql, args)
         return True if self.cursor.fetchall()[0][0] == 1 else False
 
+
+    """GAMES SECTION
+
+    CREATE TABLE "Games" (
+        "Name"	TEXT,
+        "Description"	TEXT,
+        "Location"	TEXT,
+        "Age"	TEXT,
+        "Type"	TEXT,
+        "Props"	TEXT
+    );"""
+
+    def set_game(self, Name, Description, Location, Age, Type, Props):
+        """ Inserts game to the database """
+        sql = 'INSERT INTO Games (Name, Description, Location, Age, Type, Props) VALUES (?,?,?,?,?,?)'
+        args = [Name, Description, Location, Age, Type, Props]
+        try:
+            self.cursor.execute(sql, args)
+            self.conn.commit()
+            print(f"Game {Name} inserted succsesfully")
+        except sqlite3.IntegrityError:
+            print(f"ERROR while inserting {Name}")
+            self.conn.commit()
     
-# tests
-# DbInterface('database.db')\
-#     .authorizeUser(payment_id = 1, chat_id = 1,day = 1, time = 1, username = "lol", email = "kek")
-# print(DbInterface('database.db').checkUser(1))
+    def get_games(self, Location = None, Age = None, Type = None, Props = None):
+        sql = "SELECT Name, Description FROM Games WHERE "
+        sql += f'Location LIKE \'%{Location}%\''
+        if Age is not None:
+            sql += f'AND (Age LIKE \'%{Age}%\') '
+        if Type is not None:
+            sql += f'AND (Type LIKE \'%{Type}%\')'
+        if Props is not None:
+            sql += f'AND (Props LIKE \'%{Props}%\')'
+        try:
+            self.cursor.execute(sql)#, args)
+            data = self.cursor.fetchall()
+            self.conn.commit()
+            return data
+        except:
+            self.conn.commit()
+            print(f"Your request {Location, Age, Type, Props} failed")
+
+DB = DbInterface('database.db')
+# games = DB.get_games("дома", "обучающая", "3-4", "да")
+# PrettyPrinter().pprint(games)
