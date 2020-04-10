bf45db23d971bc49831d8c7f37abf53e0bbc528e
Elon_Mask <you@example.com>
2020-04-10 19:57:53 +0300

    Minor DB improvments

--- src/database.py
+++ src/database.py
@@ -6,36 +6,9 @@ class DbInterface:
         self.conn = sqlite3.connect(path, check_same_thread = False)
         self.cursor = self.conn.cursor()
 
-    # def getGames(self, type=None, age=None, amount=None, location=None, props=None):
-    #     sql = "SELECT DISTINCT Id FROM Games WHERE "
-    #     args = [type]
-    #     sql += 'Type=?'
-    #     if age is not None:
-    #         sql += 'AND (Age=? OR Age is NULL)'
-    #         args.append(age)
-    #     if amount is not None:
-    #         sql += 'AND (Amount=? OR Amount is NULL)'
-    #         args.append(amount)
-    #     if location is not None:
-    #         sql += 'AND (Location=? OR Location is NULL)'
-    #         args.append(location)
-    #     if props is not None:
-    #         sql += 'AND (Props=? OR Props is NULL)'
-    #         args.append(props)
-    #     self.cursor.execute(sql, args)
-    #     data = self.cursor.fetchall()
-    #     return data if len(data) == 0 else tuple(d[0] for d in data)
-
-    def authorizeUser(self, payment_id, chat_id, day, time, username, email):
-        """ Inserts user to the table of paid users
-        
-        To make insertion more reliable
-        you should provide arguments by name
-        .authorizeUser(payment_id = 1, chat_id = 1,day = 1, time = 1, username = "lol", email = "kek")
-        
-        return if insertion was succesfull(True) or not (False)"""
-        sql = 'INSERT INTO Payments (payment_id, chat_id, day, time, username, email) VALUES (?,?,?,?,?,?)'
-        args = [payment_id, chat_id, day, time, username, email]
+    def add_visitor(self, chat_id):
+        sql = 'INSERT INTO Visitors (chat_id) VALUES (?)'
+        args = [chat_id]
         try:
             self.cursor.execute(sql, args)
             self.conn.commit()
@@ -44,25 +17,40 @@ class DbInterface:
             print("User exists")
             return False
 
-    def checkUser(self, chat_id):
+    def authorize_payed_user(self, payment_id, chat_id, status, create_date, end_date):
+        """ Inserts user to the table of paid users
+        
+        To make insertion more reliable
+        you should provide arguments by name
+        .authorizeUser(payment_id = 1, chat_id = 1,day = 1, time = 1, username = "lol", email = "kek")
+        
+        return if insertion was succesfull(True) or not (False)"""
+
+        sql = 'INSERT INTO Payments (payment_id, chat_id, status, create_date, end_date) VALUES (?,?,?,?,?)'
+        args = [payment_id, chat_id, status, create_date, end_date]
+        try:
+            self.cursor.execute(sql, args)
+            self.conn.commit()
+            return True
+        except sqlite3.IntegrityError:
+            print("User exists")
+            return False
+
+    def check_payed_user(self, chat_id):
         """ Checks out if provided user in our payments tables
         return boolean answer"""
-        sql = 'SELECT EXISTS(SELECT * from Payments Where Chat_id = ?)'
+        sql = 'SELECT EXISTS(SELECT * from Payments WHERE Chat_id = ?)'
         args = [chat_id]
-        self.cursor.execute(sql, args)
-        return True if self.cursor.fetchall()[0][0] == 1 else False
+        try:
+            self.cursor.execute(sql, args)
+            self.conn.commit()
+            return True if self.cursor.fetchall()[0][0] == 1 else False
+        except sqlite3.IntegrityError:
+            print("ERROR while checking the user")
+            return False
 
 
-    """GAMES SECTION
-
-    CREATE TABLE "Games" (
-        "Name"	TEXT,
-        "Description"	TEXT,
-        "Location"	TEXT,
-        "Age"	TEXT,
-        "Type"	TEXT,
-        "Props"	TEXT
-    );"""
+    """GAMES SECTION"""
 
     def set_game(self, Name, Description, Location, Age, Type, Props):
         """ Inserts game to the database """
@@ -95,5 +83,5 @@ class DbInterface:
             print(f"Your request {Location, Age, Type, Props} failed")
 
 DB = DbInterface('database.db')
-# games = DB.get_games("дома", "обучающая", "3-4", "да")
-# PrettyPrinter().pprint(games)
+DB.add_visitor(2)
+# print(DB.check_payed_user(2))
